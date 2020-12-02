901bc520c97d0bf6a49dcc9b39148edcbfd51e15
Fakhr shaheen <fakhrshaheen@gmail.com>
2020-12-02 12:25:46 +0100

    fix small bug

--- include/Dice/Sparql-Query/QueryNodes/GroupNodes/OptionalPatternNode.hpp
+++ include/Dice/Sparql-Query/QueryNodes/GroupNodes/OptionalPatternNode.hpp
@@ -5,4 +5,18 @@
 #ifndef SPARQL_QUERY_OPTIONALPATTERNNODE_HPP
 #define SPARQL_QUERY_OPTIONALPATTERNNODE_HPP
 
+#include "GroupNode.hpp"
+
+class OptionalPatternNode :public GroupNode
+        {
+                public:
+                SelectQueryResult execute(const SelectQueryResult& previousQueryResult) override {
+                    SelectQueryResult queryResult=previousQueryResult;
+                    for(auto child:children)
+                        queryResult=child->execute(queryResult);
+                    //ToDo implement the logic here to perform : return queryResult=previousQueryResult-queryResult
+                    return queryResult;
+                }
+        };
+
 #endif //SPARQL_QUERY_OPTIONALPATTERNNODE_HPP
