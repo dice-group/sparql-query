ed4e9c352637f65b2918b219d06de92cdda8bfd6
Fakhr shaheen <fakhrshaheen@gmail.com>
2021-01-08 15:03:42 +0100

    delelte unused file

--- tests/generalTests.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <Dice/Sparql-Query/TriplePatternElement.hpp>
-#include "Dice/Sparql-Query/QueryNodes/leafNodes/TriplePatternNode.hpp"
-#include <Dice/Sparql-Query/QueryNodes/SpecialNodes/OptionalPatternNode.hpp>
-#include <Dice/Sparql-Query/QueryNodes/GroupNode.hpp>
-TEST(GrammerTests, junk) {
-
-}
